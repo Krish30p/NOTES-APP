a91ee7d241109e6ac081992948f814e34e5c54a7
KrishnaPatel <kp06krish@gmail.com>
2025-10-15 02:10:33 +0530

    Add MongoDB connection and user/note models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv  from 'dotenv';
+import { connectDB } from './config/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
@@ -12,6 +13,7 @@ app.get('/', (req , res)  => {
 
 })
 
+connectDB();
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 
